78599ac6390680d9a0c4da333e1afe9773e7113c
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-02-23 13:56:34 -0500

    Import LayoutModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthModule } from "./components/auth/auth.module";
+import { LayoutModule } from "./components/layout/layout.module";
 import { ModelModule } from "./models/model.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, ModelModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    LayoutModule,
+    ModelModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
