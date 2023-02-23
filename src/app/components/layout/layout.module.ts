402409172a57f79dbd5357f71a83573610f010b4
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-02-23 13:54:50 -0500

    Create LayoutModule to govern layout components

---
+++ src/app/components/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { DefaultLayoutComponent } from "./default/default.component";
+import { EmptyLayoutComponent } from "./empty/empty.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: [
+    DefaultLayoutComponent,
+    EmptyLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  exports: [DefaultLayoutComponent, EmptyLayoutComponent, HeaderComponent, FooterComponent],
+})
+export class LayoutModule {}
