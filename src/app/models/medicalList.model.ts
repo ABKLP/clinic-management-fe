031cf8e52e69bab9cefc6d42b9e26f9aa30e8400
nanashi02 <39255101+nanashi02@users.noreply.github.com>
2023-03-05 22:08:29 -0500

    Added_Authguard
    Still having an issue -> Signin when update information has been activated.

---
+++ src/app/models/medicalList.model.ts
@@ -0,0 +1,9 @@
+export class MedicalList {
+  constructor(
+    public _id?: string,
+    public findings?: string,
+    public medicine?: string,
+    public recordedDate?: string,
+    public doctorName?: string,
+  ) { }
+}
