606fba4ae494b716550f6a1aeb6799d93ebb332d
Hajibandeh <113070569+Hajibandeh@users.noreply.github.com>
2023-03-06 11:07:36 -0500

    add html schedule component

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { AuthService } from "src/app/models/auth.service";
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+
+export class SidebarComponent implements OnInit {
+  @Input() title?: string;
+
+
+  sideTitles = [
+    "Account Information",
+    "Medical History",
+    "Schedule",
+  ]
+  constructor(public auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+}
