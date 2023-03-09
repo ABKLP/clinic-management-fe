7959a0da21a21eac2518d917657021e7b9695d3c
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-03-08 23:02:06 -0500

    Add user list route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AuthComponent } from "./components/auth/auth.component";
+import { AuthGuard } from "./components/auth/auth.guard";
 import { RegisterComponent } from "./components/auth/register.component";
 import { IndexComponent } from "./components/index/index.component";
 import { DefaultLayoutComponent } from "./components/layout/default/default.component";
 import { EmptyLayoutComponent } from "./components/layout/empty/empty.component";
+import { UserListComponent } from "./components/user/list/list.component";
 
-const routesDefaultLayout: Routes = [{ path: "", component: IndexComponent }];
+const routesDefaultLayout: Routes = [
+  { path: "", component: IndexComponent },
+  {
+    path: "users/list",
+    component: UserListComponent,
+    canActivate: [AuthGuard],
+  },
+];
 
 const routesEmptyLayout: Routes = [
   {
