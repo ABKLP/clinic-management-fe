68d179b03a11ad81eed9fbff0036432510333c28
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-03-28 09:51:32 -0400

    Move SidebarComponent to layout

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,31 +1,26 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { AuthService } from "src/app/models/auth.service";
-import { ActivatedRoute } from '@angular/router';
-import { MedicalRecord } from "../models/medical-record.model";
-import { MedicalRecordRepository } from "../models/medical-record.repository";
+import { ActivatedRoute } from "@angular/router";
+import { MedicalRecord } from "../../../models/medical-record.model";
+import { MedicalRecordRepository } from "../../../models/medical-record.repository";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
-
 export class SidebarComponent implements OnInit {
   @Input() title?: string;
   editing: boolean = false;
- 
+
   _medicalRecord: MedicalRecord = new MedicalRecord();
 
-  sideTitles = [
-    "Account Information",
-    "Medical Record",
-    "Add Medical Record",
-  ]
+  sideTitles = ["Account Information", "Medical Record", "Add Medical Record"];
   constructor(
     private repository: MedicalRecordRepository,
     public auth: AuthService,
-    private activeRoute: ActivatedRoute) { }
-
+    private activeRoute: ActivatedRoute
+  ) {}
 
   async ngOnInit(): Promise<void> {
     await this.repository.setMedicalRecord();
@@ -34,8 +29,9 @@ export class SidebarComponent implements OnInit {
 
     // Edit
     if (this.editing) {
-      this._medicalRecord = this.repository.getItem(this.activeRoute.snapshot.params["id"]);
+      this._medicalRecord = this.repository.getItem(
+        this.activeRoute.snapshot.params["id"]
+      );
     }
   }
-
 }
