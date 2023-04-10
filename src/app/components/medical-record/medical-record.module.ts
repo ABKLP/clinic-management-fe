ad55414f890643c4471b6e9b2ced2d862fae4f74
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-04-09 20:30:49 -0400

    Remove unused components

--- src/app/components/medical-record/medical-record.module.ts
+++ src/app/components/medical-record/medical-record.module.ts
@@ -6,10 +6,7 @@ import { NgbTypeaheadModule } from "@ng-bootstrap/ng-bootstrap";
 import { LayoutModule } from "../layout/layout.module";
 import { SharedModule } from "../shared/shared.module";
 import { MedicalRecordListComponent } from "./list/list.component";
-import { MedicalRecordComponent } from "src/app/medical-record/medical-record.component";
-import { SearchPageComponent } from "src/app/search-page/search-page.component";
 import { MedicalRecordAddEditComponent } from "./add-edit/add-edit.component";
-import { AddEditComponent } from "src/app/add-edit/add-edit.component";
 import { MedicalRecordSearchComponent } from "./search/search.component";
 
 @NgModule({
@@ -23,19 +20,13 @@ import { MedicalRecordSearchComponent } from "./search/search.component";
   ],
   declarations: [
     MedicalRecordListComponent,
-    MedicalRecordComponent,
     MedicalRecordSearchComponent,
-    SearchPageComponent,
     MedicalRecordAddEditComponent,
-    AddEditComponent,
   ],
   exports: [
     MedicalRecordListComponent,
-    MedicalRecordComponent,
     MedicalRecordSearchComponent,
-    SearchPageComponent,
     MedicalRecordAddEditComponent,
-    AddEditComponent,
   ],
 })
 export class MedicalRecordModule {}
